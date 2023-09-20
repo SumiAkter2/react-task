baacf69e099d5c2fd52f4f022380b92573fd35c0
SumiAkter2 <akteralisha36@gmail.com>
2023-09-20 08:28:51 +0600

    solve the problem-1

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,22 @@ import Problem1 from "./components/Problem-1.jsx";
 import Menu from "./components/Menu.jsx";
 import Problem2 from "./components/Problem-2.jsx";
 import Index from "./components/Index.jsx";
+import { useState } from "react";
 
 function App() {
+   const [tasks, setTasks] = useState([]);
+   const addTask = (task) => {
+     setTasks([...tasks, task]);
+   };
   return (
     <>
       <Routes>
         <Route path="/" element={<Index />} />
         <Route path="/" element={<Menu />}>
-          <Route path="problem-1" element={<Problem1 />} />
+          <Route
+            path="problem-1"
+            element={<Problem1 onAddTask={addTask} tasks={tasks} />}
+          />
           <Route path="problem-2" element={<Problem2 />} />
         </Route>
       </Routes>
--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -1,59 +1,119 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-const Problem1 = () => {
+const Problem1 = ({ onAddTask, tasks }) => {
+  const [show, setShow] = useState("");
+  const [name, setName] = useState("");
+  const [status, setStatus] = useState("");
 
-    const [show, setShow] = useState('all');
-
-    const handleClick = (val) =>{
-        setShow(val);
+  const handleClick = (e) => {
+    e.preventDefault();
+    if (name.trim() !== "" && status.trim() !== "") {
+      onAddTask({ name, status });
+      setName("");
+      setStatus("");
     }
+  };
 
-    return (
+  const [filter, setFilter] = useState("All");
 
-        <div className="container">
-            <div className="row justify-content-center mt-5">
-                <h4 className='text-center text-uppercase mb-5'>Problem-1</h4>
-                <div className="col-6 ">
-                    <form className="row gy-2 gx-3 align-items-center mb-4">
-                        <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Name"/>
-                        </div>
-                        <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Status"/>
-                        </div>
-                        <div className="col-auto">
-                            <button type="submit" className="btn btn-primary">Submit</button>
-                        </div>
-                    </form>
-                </div>
-                <div className="col-8">
-                    <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
-                        <li className="nav-item">
-                            <button  className={`nav-link ${show==='all' && 'active'}`} type="button" onClick={()=>handleClick('all')}>All</button>
-                        </li>
-                        <li className="nav-item">
-                            <button className={`nav-link ${show==='active' && 'active'}`} type="button" onClick={()=>handleClick('active')}>Active</button>
-                        </li>
-                        <li className="nav-item">
-                            <button  className={`nav-link ${show==='completed' && 'active'}`} type="button" onClick={()=>handleClick('completed')}>Completed</button>
-                        </li>
-                    </ul>
-                    <div className="tab-content"></div>
-                    <table className="table table-striped ">
-                        <thead>
-                        <tr>
-                            <th scope="col">Name</th>
-                            <th scope="col">Status</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        
-                        </tbody>
-                    </table>
-                </div>
+  const filteredTasks = tasks.filter((task) =>
+    filter === "All" ? true : task.status === filter
+  );
+
+  const sortedTasks = filteredTasks.sort((a, b) => {
+    if (a.status === "active") return -1;
+    if (b.status === "active") return 1;
+    if (a.status === "completed") return -1;
+    if (b.status === "completed") return 1;
+    return 0;
+  });
+
+  return (
+    <div className="container">
+      <div className="row justify-content-center mt-5">
+        <h4 className="text-center text-uppercase mb-5">Problem-1</h4>
+        <div className="col-6 ">
+          <form
+            onSubmit={handleClick}
+            className="row gy-2 gx-3 align-items-center mb-4"
+          >
+            <div className="col-auto">
+              <input
+                onBlur={(e) => setName(e.target.value)}
+                type="text"
+                className="form-control"
+                placeholder="Name"
+              />
             </div>
+            <div className="col-auto">
+              <input
+                onBlur={(e) => setStatus(e.target.value)}
+                type="text"
+                className="form-control"
+                placeholder="Status"
+              />
+            </div>
+            <div className="col-auto">
+              <button type="submit" className="btn btn-primary">
+                Submit
+              </button>
+            </div>
+          </form>
         </div>
-    );
+        <div className="col-8">
+          <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
+            <li className="nav-item">
+              <button
+                className={`nav-link ${show === "all" && "active"}`}
+                type="button"
+                // onClick={() => handleClick("all")}
+                onClick={() => setFilter("All")}
+              >
+                All
+              </button>
+            </li>
+            <li className="nav-item">
+              <button
+                className={`nav-link ${show === "active" && "active"}`}
+                type="button"
+                // onClick={() => handleClick("active")}
+                onClick={() => setFilter("active")}
+              >
+                Active
+              </button>
+            </li>
+            <li className="nav-item">
+              <button
+                className={`nav-link ${show === "completed" && "active"}`}
+                type="button"
+                // onClick={() => handleClick("completed")}
+                onClick={() => setFilter("completed")}
+              >
+                Completed
+              </button>
+            </li>
+          </ul>
+          <div className="tab-content"></div>
+          <table className="table table-striped ">
+            <thead>
+              <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Status</th>
+              </tr>
+            </thead>
+            <tbody>
+              {sortedTasks.map((task, index) => (
+                <tr key={index}>
+                  <td>{task.name}</td>
+                  <td>{task.status}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Problem1;
